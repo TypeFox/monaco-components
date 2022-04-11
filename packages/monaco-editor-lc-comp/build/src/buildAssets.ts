e26cf280cec141fe14293ac6b571a7c5b92ad2f8
Kai Salmen <kai.salmen@typefox.io>
2022-04-11 16:46:59 +0200

    Fix css generation

--- packages/monaco-editor-lc-comp/build/src/buildAssets.ts
+++ packages/monaco-editor-lc-comp/build/src/buildAssets.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
 
-fs.readFile('../../../node_modules/monaco-editor/min/vs/editor/editor.main.css', 'utf8' , (err, data) => {
+fs.readFile('../../node_modules/monaco-editor/min/vs/editor/editor.main.css', 'utf8' , (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
-    fs.writeFileSync('../src/generated/css.ts', `export function getMonacoCss() {\n    return \`${data}\`;\n}`);
+    fs.mkdirSync('./src/generated');
+    fs.writeFileSync('./src/generated/css.ts', `export function getMonacoCss() {\n    return \`${data}\`;\n}`);
 });
