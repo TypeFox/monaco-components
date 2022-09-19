c9883a9afc3a2dd5f96dea30b3e32f20513ddb16
Kai Salmen <kai.salmen@typefox.io>
2022-09-19 15:41:57 +0200

    Allow to set message reader and writer for worker

---
+++ packages/examples/src/wrapper.ts
@@ -0,0 +1,91 @@
+import { buildWorkerDefinition } from 'monaco-editor-workers';
+buildWorkerDefinition('../../../node_modules/monaco-editor-workers/dist/workers', import.meta.url, false);
+import { MonacoEditorLanguageClientWrapper } from 'monaco-editor-wrapper';
+import { BrowserMessageReader, BrowserMessageWriter } from 'vscode-languageserver/browser.js';
+
+// helper functions for adding Codicon TTF to document and monaco styles to head
+MonacoEditorLanguageClientWrapper.addMonacoStyles('monaco-editor-styles');
+MonacoEditorLanguageClientWrapper.addCodiconTtf();
+const client = new MonacoEditorLanguageClientWrapper();
+
+const languageId = 'plaintext';
+const codeMain = `#ff0000 (red)
+#00ff00 (green)
+#0000ff (blue)`;
+const codeDiff = `#ffff00 (yellow)
+#00ff00 (green)
+#0000ff (blue)`;
+let toggleDiff = true;
+
+function startEditor() {
+    if (client.isStarted()) {
+        alert('Editor was already started!');
+        return;
+    }
+
+    const editorConfig = client.getEditorConfig();
+    editorConfig.setLanguageExtensionConfig({
+        id: 'plaintext',
+        extensions: ['.txt'],
+        aliases: ['PLAINTEXT', 'plaintext'],
+        mimetypes: ['text/plain']
+    });
+    editorConfig.setMainLanguageId(languageId);
+    editorConfig.setMainCode(codeMain);
+    editorConfig.setTheme('vs-dark');
+
+    editorConfig.setUseLanguageClient(true);
+    editorConfig.setUseWebSocket(false);
+    const workerURL = new URL('./src/serverWorker.ts', window.location.href).href;
+    console.log(workerURL);
+
+    const lsWorker = new Worker(workerURL, {
+        type: 'module',
+        name: 'LS'
+    });
+    const reader = new BrowserMessageReader(lsWorker);
+    const writer = new BrowserMessageWriter(lsWorker);
+    client.setWorker(lsWorker, { reader: reader, writer: writer });
+    //client.setWorker(lsWorker);
+
+    client.startEditor(document.getElementById('monaco-editor-root') as HTMLElement)
+        .then((s: unknown) => console.log(s))
+        .catch((e: Error) => console.error(e));
+
+    // provoke a too early dispose
+    setTimeout(() => {
+        client.dispose()
+            .then((s: unknown) => console.log(s))
+            .catch((e: Error) => console.error(e));
+    }, 10);
+}
+
+function swapEditors() {
+    const editorConfig = client.getEditorConfig();
+    editorConfig.setUseDiffEditor(toggleDiff);
+    editorConfig.setMainLanguageId(languageId);
+    editorConfig.setMainCode(codeMain);
+    editorConfig.setDiffLanguageId(languageId);
+    editorConfig.setDiffCode(codeDiff);
+
+    client.swapEditors(document.getElementById('monaco-editor-root') as HTMLElement)
+        .then((s: string) => {
+            toggleDiff = !toggleDiff;
+            console.log(s);
+        })
+        .catch((e: Error) => console.error(e));
+}
+
+async function disposeEditor() {
+    client.reportStatus();
+    await client.dispose()
+        .then(() => {
+            client.reportStatus();
+        });
+}
+
+document.querySelector('#button-start')?.addEventListener('click', startEditor);
+document.querySelector('#button-swap')?.addEventListener('click', swapEditors);
+document.querySelector('#button-dispose')?.addEventListener('click', disposeEditor);
+
+startEditor();
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                monacoEditorWrapper: path.resolve(__dirname, '/packages/monaco-editor-wrapper/index.html'),
+                monacoEditorWrapper: path.resolve(__dirname, '/packages/examples/wrapper.html'),
                 monacoEditorComp: path.resolve(__dirname, '/packages/monaco-editor-comp/index.html'),
                 monacoEditorCompAdvanced: path.resolve(__dirname, '/packages/monaco-editor-comp/advanced.html'),
                 monacoEditorWorkers: path.resolve(__dirname, '/packages/monaco-editor-workers/index.html'),
